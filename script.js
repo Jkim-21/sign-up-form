041260790a1a5a58ccea2a961466f153d299a7b8
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-12-17 20:53:32 -0500

    Complete form styling and validation
    The form has full client-side validation in case the user enters the
    incorrect format of their information, specifically with their phone
    number, email, and confirm password.

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputs = document.querySelectorAll("input");
+const form = document.querySelector("#user-form");
+const errorMessages = document.querySelectorAll(".error");
+const submitButton = document.querySelector(".submit-form");
+const password = document.querySelector("#user-password");
+
+
+for (let i = 0; i < inputs.length; i++) {
+    let currentInput = inputs[i];
+    let currentError = errorMessages[i];
+    currentInput.addEventListener("keyup", () => {
+       currentError.textContent = validationChecker(currentInput);
+    });
+}
+
+
+submitButton.addEventListener("click", (e) => {
+    inputs.forEach(input => {
+        if (!input.checkValidity()) {
+            e.preventDefault();
+            input.nextElementSibling.textContent = validationChecker(input);
+        }
+        if (!input.value) {
+            input.style.border = '1px solid red';
+        }
+    });
+});
+
+function validationChecker(input) {
+    let numbers = /^[0-9]+$/;
+
+    if (input.value === "") {
+        return "This Field is Required";
+    }
+    
+    else if (input.type === "tel" && !input.value.match(numbers)) {
+        return "Please Enter a Valid Number";
+    }
+
+    else if (input.type === "email" && !input.checkValidity()) {
+        return "Please Enter a Valid Email Address";
+    }
+
+    else if (input.id === "user-password-confirm" && input.value !== password.value) {
+        return "Password Does Not Match";
+    }
+    else {
+        return "";
+    }
+}
